c641910651e88eab6f67046ca044c93d81c1fed7
Nithya Sam <nithyasam@gmail.com>
2018-02-10 17:35:19 -0600

    displaying the data on the menu component

--- app/assets/javascripts/components/menu.es6.jsx
+++ app/assets/javascripts/components/menu.es6.jsx
@@ -1,20 +1,32 @@
 class Menu extends React.Component {
-  render () {
-    return (
-      <div>
-        <div>Date: {this.props.date}</div>
-        <div>Name: {this.props.name}</div>
-        <div>Is Present: {this.props.isPresent}</div>
-        <div>Price: {this.props.price}</div>
-      </div>
-    );
-  }
-}
 
-Menu.propTypes = {
-  date: PropTypes.node,
-  name: PropTypes.node,
-  isPresent: PropTypes.node,
-  price: PropTypes.node
-};
+    render () {
+        return (
+            <div>
+                <table style={{border: "1px solid black"}}>
+                    <thead>
+                    <tr>
+                        <th style={{border: "1px solid black"}}>Date</th>
+                        <th style={{border: "1px solid black"}}>Name</th>
+                        <th style={{border: "1px solid black"}}>Is Present</th>
+                        <th style={{border: "1px solid black"}}>Price</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {this.props.data.map(item => {
+                        return (
+                            <tr key={item.name}>
+                                <td style={{border: "1px solid black"}}>{item.date}</td>
+                                <td style={{border: "1px solid black"}}>{item.name}</td>
+                                <td style={{border: "1px solid black"}}>{item.isPresent.toString()}</td>
+                                <td style={{border: "1px solid black"}}>{item.price}</td>
+                            </tr>
+                        )
+                    })}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
 
